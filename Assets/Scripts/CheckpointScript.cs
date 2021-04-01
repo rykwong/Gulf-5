a342a22ff525bb53eb42de10d31786e3b98d3f0b
someguy13 <ryan.kwong@gmail.com>
2021-04-01 14:14:45 -0700

    tutorial level

---
+++ Assets/Scripts/CheckpointScript.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class CheckpointScript : MonoBehaviour
+{
+
+    public TextMeshProUGUI uiText;
+    public int index;
+
+
+    void Start()
+    {
+        uiText.enabled = false; 
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            uiText.enabled = true;
+            
+
+        }
+    }
+
+   // void OnCollisionExit2D(Collision2D collision)
+   // {
+   //      if(collision.gameObject.tag == "Player")
+   //      {
+   //         uiText.enabled = false;
+   //         
+   //     }
+   // }
+}
---
+++ Assets/Scripts/TextDialog.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+using System.Reflection;
+
+public class TextDialog : MonoBehaviour
+{
+    public TextMeshProUGUI textDisplay;
+    public string[] sentences;
+    private int index;
+    public float typingSpeed;
+    public bool triggered = true;
+    public float delay;
+
+    void Update()
+    {
+        if (textDisplay.enabled == true && triggered == true) 
+        {
+            StartCoroutine(Type());
+            triggered = false;
+
+        }
+
+        
+    }
+    IEnumerator Type()
+    {
+        foreach(char letter in sentences[index].ToCharArray())
+        {
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+        yield return new WaitForSeconds(delay);
+        textDisplay.enabled = false;
+    }
+}
