2b6232f13e430a48049d1f2cdc46952b93fef169
someguy13 <ryan.kwong@gmail.com>
2021-04-03 11:20:31 -0700

    pause menu

---
+++ Assets/Scripts/MainMenu/PauseMenu.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+    void Update()
+    {
+        checkKey();
+    }
+
+    public void checkKey()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape) && GetComponent<SceneTransitions>().anim.GetCurrentAnimatorStateInfo(0).IsName("idle"))
+        {
+            if(GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+}
