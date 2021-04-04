4c24c74bb3002428219e672af59842133d92c395
someguy13 <ryan.kwong@gmail.com>
2021-04-04 12:21:53 -0700

    level boundaries

--- Assets/Scripts/EndlessPit.cs
+++ Assets/Scripts/EndlessPit.cs
@@ -1,7 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EndlessPit : MonoBehaviour
 {
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            other.transform.position = GameObject.Find("Start").transform.position;
+        }
+        else if (other.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
 }
